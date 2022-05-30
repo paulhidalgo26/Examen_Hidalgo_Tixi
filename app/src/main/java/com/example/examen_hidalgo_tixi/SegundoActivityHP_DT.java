f95c977e7efc6840813b58176162f231447e6084
paulhidalgo26 <paulsb26@gmail.com>
2022-05-30 18:36:02 -0500

    Primer 6:30

---
+++ app/src/main/java/com/example/examen_hidalgo_tixi/SegundoActivityHP_DT.java
@@ -0,0 +1,14 @@
+package com.example.examen_hidalgo_tixi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegundoActivityHP_DT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundo_hp_dt);
+    }
+}
