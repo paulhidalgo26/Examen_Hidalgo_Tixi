2ee8c7ef64a56049bf649a7405ccefd765c9d55e
dario <dario.moya2016@gmail.com>
2022-05-30 19:23:55 -0500

    Guardar

--- app/src/main/java/com/example/examen_hidalgo_tixi/SegundoActivityHP_DT.java
+++ app/src/main/java/com/example/examen_hidalgo_tixi/SegundoActivityHP_DT.java
@@ -2,17 +2,33 @@ package com.example.examen_hidalgo_tixi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SegundoActivityHP_DT extends AppCompatActivity {
-
+private EditText edt;
+private ListView lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo_hp_dt);
+    edt= findViewById(R.id.valorIngresado);
+    lista= findViewById(R.id.listviewValores);
+
     }
 
-    public void Cerrar(View view){
+    public void CerrarHP_DT(View view){
+        Intent intent = new Intent(this,MainActivityHP_DT.class);
+        startActivity(intent);
+    }
+    public  void Ingresar(View view){
+
 
     }
 }
