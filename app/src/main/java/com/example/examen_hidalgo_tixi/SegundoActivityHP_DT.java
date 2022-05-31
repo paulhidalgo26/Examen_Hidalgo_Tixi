20cd4f0b8dc5d2e3fa04a5360a2546233b4949a4
paulhidalgo26 <paulsb26@gmail.com>
2022-05-30 19:39:01 -0500

    pasarEntreActividadesDos

--- app/src/main/java/com/example/examen_hidalgo_tixi/SegundoActivityHP_DT.java
+++ app/src/main/java/com/example/examen_hidalgo_tixi/SegundoActivityHP_DT.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -12,22 +13,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SegundoActivityHP_DT extends AppCompatActivity {
-private EditText edt;
-private ListView lista;
+    private EditText edt;
+    private ListView lista;
+    private Button cerar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo_hp_dt);
-    edt= findViewById(R.id.valorIngresado);
-    lista= findViewById(R.id.listviewValores);
+        edt=findViewById(R.id.Tx_insertar);
+        cerar=findViewById(R.id.buttonCerrar);
+        lista= findViewById(R.id.Valores);
 
     }
 
-    public void CerrarHP_DT(View view){
-        Intent intent = new Intent(this,MainActivityHP_DT.class);
-        startActivity(intent);
+    public void CerrarHP_DT(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("resultado",edt.getText().toString());
+        setResult(70, intent );
+        finish();
+
+        SegundoActivityHP_DT.super.onBackPressed();
     }
-    public  void Ingresar(View view){
+
+    public void Ingresar(View view) {
 
 
     }
